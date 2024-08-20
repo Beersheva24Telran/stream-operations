f5778daa1d65e4cc2e35c2d76978535c011a3c12
Yuragranovsky <yuragranovsky@gmail.com>
2024-08-20 13:28:49 +0300

    HW #20 solution "shuffle"

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -1,12 +1,12 @@
 package telran.stream;
 
+import java.util.Random;
+
 public class StreamTasks {
 public static int[] shuffle(int [] arr) {
-    //TODO
-    //only one pipeline for getting new array of int's
-    //with all numbers from a given array but with different order
-    //each method call returns new array in some random order
-    return null;
+    
+    return new Random().ints(0, arr.length).distinct().limit(arr.length)
+    .map(i -> arr[i]).toArray();
 
 }
 }
--- src/test/java/telran/stream/StreamTasksTest.java
+++ src/test/java/telran/stream/StreamTasksTest.java
@@ -1,11 +1,31 @@
 package telran.stream;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public class StreamTasksTest {
- @Test
+ private static final int N_RUNS = 100;
+
+@Test
  void suffleTest() {
-    //TODO
-    //testing of method shuffle of StreamTasks class
+    int array[] = {10, 10, 20, 30, 40, 50, 50, 70, 80, 90};
+    int[] arShuffled = StreamTasks.shuffle(array);
+    for (int i = 1; i < N_RUNS; i++ ) {
+        int[] arShuffled2 = StreamTasks.shuffle(array);
+        assertFalse(Arrays.equals(arShuffled, arShuffled2));
+        arShuffled = arShuffled2;
+        runTest(arShuffled, array);
+    }
+    
+    
  }
+
+private void runTest(int[] shuffled, int[] array) {
+    assertFalse(Arrays.equals(shuffled, array));
+    Arrays.sort(shuffled);
+    assertTrue(Arrays.equals(shuffled, array));
+}
 }
